3a06a70fc7e926177e3f5d73f7c244877cc90065
maggiejjgreene <maggiejjgreen@gmail.com>
2021-04-13 22:44:06 +0100

    Initialized my repo and added m1c1

---
+++ m1c1.py
@@ -0,0 +1,39 @@
+# Example 1, do not modify!
+print(8 / 2)
+
+# Example 2, do not modify!
+print(2**2)
+
+print(10+2)
+var = 12
+# Addition, no print output
+
+
+total = revenue_1 + revenue_2 + revenue_3
+print(revenue_1 + revenue_2 + revenue_3)
+
+# Create company_1
+company_1 = 'Apple'
+print(company_1)
+
+# Type of company_1
+print(____(company_1))
+
+# Type of year_1
+____(____(year_1))
+
+# Type of revenue_1
+____
+
+test_company = 'apple'
+print(company_1 == test_company)
+
+# Update data types
+year_1_str = str(year_1)
+revenue_1_str = str(revenue_1)
+
+# Create a complete setence combining only the string data types
+sentence = 'The revenue of ' + company_1 + ' in ' + year_1_str + ' was $' + revenue_1_str + ' billion.'
+
+# Print sentence
+print(sentence)
