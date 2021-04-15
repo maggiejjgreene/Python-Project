563ee4eeb29195883eb8585885559a3e1e0cf196
maggiejjgreene <maggiejjgreen@gmail.com>
2021-04-15 21:43:34 +0100

    updated my test m1c1

--- m1c1.py
+++ m1c1.py
@@ -1,3 +1,7 @@
+import pandas as pd
+data = pd.read_csv("stock_data.csv")
+print(data.info())
+
 # Example 1, do not modify!
 print(8 / 2)
 
@@ -8,25 +12,46 @@ print(10+2)
 var = 12
 # Addition, no print output
 
+print(10*2)
+20
 
 total = revenue_1 + revenue_2 + revenue_3
 print(revenue_1 + revenue_2 + revenue_3)
+
+# Print the total revenue
+total = revenue_1 + revenue_2 + revenue_3
+print(total)
+
+# Print the average revenue
+average = total / 3
+print(total/3)
 
 # Create company_1
 company_1 = 'Apple'
 print(company_1)
 
+# Create year_1
+year_1 = 2017
+print(year_1)
+
+# Create revenue_1
+revenue_1 = 229.23
+print(revenue_1)
+
 # Type of company_1
-print(____(company_1))
+print(type(company_1))
 
 # Type of year_1
-____(____(year_1))
+print(type(year_1))
 
 # Type of revenue_1
-____
+print(type(revenue_1))
+
 
 test_company = 'apple'
 print(company_1 == test_company)
+# Compare revenue_1 and revenue_2
+print(revenue_1 > revenue_2)
 
 # Update data types
 year_1_str = str(year_1)
